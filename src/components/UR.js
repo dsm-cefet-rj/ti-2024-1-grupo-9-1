2d20f19226c194f637a80438651024a4c77b847c
Anthony Heimlich <anthonyheimlich@hotmail.com>
2024-09-09 20:21:27 -0300

    Home

--- src/components/UR.js
+++ src/components/UR.js
@@ -1,69 +1,60 @@
-import styles from './UR.module.css'
-import {useTranslation} from "react-i18next"
-import {Link} from 'react-scroll'
-import diamond from '../assets/img/diamond.png'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import styles from './UR.module.css';
+import { useTranslation } from "react-i18next";
+import { Link } from 'react-scroll';
+import diamond from '../assets/img/diamond.png';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 import gray from "../assets/img/gray.png";
 
-import porco from "../assets/img/porco.png";
-import poivre from "../assets/img/poivre.png";
-import petit from "../assets/img/petit.png";
-
-function UR(){
-    const [t, i18n] = useTranslation("global");
-
-    const [slideIndex, setSlideIndex] = useState(0);
+function UR() {
+    const [t] = useTranslation("global");
+    const [recipes, setRecipes] = useState([]);
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            showSlides();
-        }, 2000);
+        const fetchRandomRecipes = async () => {
+            try {
+                const token = localStorage.getItem('token');
+                const response = await axios.get('http://localhost:5000/recipes', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                const allRecipes = response.data;
 
-        return () => clearInterval(interval);
-    }, [slideIndex]);
+                // Embaralhar receitas e pegar as 3 primeiras
+                const shuffled = allRecipes.sort(() => 0.5 - Math.random());
+                const selectedRecipes = shuffled.slice(0, 3);
 
-    const showSlides = () => {
-        setSlideIndex((prevIndex) => {
-            if (prevIndex === 2) {
-                return 0;
-            } else {
-                return prevIndex + 1;
+                setRecipes(selectedRecipes);
+            } catch (error) {
+                console.error('Error fetching recipes:', error);
             }
-        });
-    };
+        };
+
+        fetchRandomRecipes();
+    }, []);
 
     return (
         <section id='UR' className={styles.ur}>
-            <img className={styles.icon} src={diamond} alt="other"/>
-            <h1 className={styles.pageTitle} style={{cursor:"auto"}}>{t("UR.title")}</h1>
+            <img className={styles.icon} src={diamond} alt="diamond"/>
+            <h1 className={styles.pageTitle} style={{ cursor: "auto" }}>{t("UR.title")}</h1>
             <div className={styles.container}>
-                <div className={styles.urContainer}>
-                    <a href="/receita1" className={styles.logo}>
-                        <img src={poivre} className={styles.urImage} alt="Receita 1"/>
-                        <h3>{t("UR.receita1")}</h3>
-                    </a>
-                </div>
-                <div className={styles.urContainer}>
-                    <a href="/receita2" className={styles.logo}>
-                        <img src={porco} className={styles.urImage} alt="Receita 2"/>
-                        <h3>{t("UR.receita2")}</h3>
-                    </a>
-                </div>
-                <div className={styles.urContainer}>
-                    <a href="/receita3" className={styles.logo}>
-                        <img src={petit} className={styles.urImage} alt="Receita 3"/>
-                        <h3>{t("UR.receita3")}</h3>
-                    </a>
-                </div>
+                {recipes.map((recipe) => (
+                    <div key={recipe._id} className={styles.urContainer}>
+                        <a href={`/recipes/${recipe._id}`} className={styles.logo}>
+                            <img src={recipe.imageUrl || gray} className={styles.urImage} alt={recipe.title} />
+                            <h3>{recipe.title}</h3>
+                        </a>
+                    </div>
+                ))}
             </div>
-
             <Link to="Contato" spy={true} smooth={true} offset={0} duration={1000} className={styles.containerArrowDown}>
                 <FontAwesomeIcon icon={faAngleDown} className={styles.arrowDown}/>
             </Link>
         </section>
-    )
+    );
 }
 
-export default UR
+export default UR;
